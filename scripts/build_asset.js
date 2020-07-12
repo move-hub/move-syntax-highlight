44776288fae4f2c80a165c29eb7d930927e3e736
caojiafeng <funfriendcjf@gmail.com>
2020-07-12 19:31:15 +0800

    chore: fix build asset

--- scripts/build_asset.js
+++ scripts/build_asset.js
@@ -3,6 +3,7 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const { exit } = require('process');
 
 const parsersDir = __dirname + "/../parsers";
 if (!fs.existsSync(parsersDir)) {
@@ -17,11 +18,17 @@ function build_wasm(lang, modulePath) {
   console.log("Compiling " + lang + " parser");
   exec("node_modules/.bin/tree-sitter build-wasm " + modulePath,
     (err) => {
-      if (err) { console.log("Failed to build wasm for " + lang + ": " + err.message); }
+      if (err) {
+        console.log("Failed to build wasm for " + lang + ": " + err.message);
+        exit(1);
+      }
       else {
         fs.rename(output, "parsers/" + lang + ".wasm",
           (err) => {
-            if (err) { console.log("Failed to copy built parser: " + err.message); }
+            if (err) {
+              console.log("Failed to copy built parser: " + err.message);
+              exit(1);
+            }
             else { console.log("Successfully compiled " + lang + " parser"); }
           });
       }
